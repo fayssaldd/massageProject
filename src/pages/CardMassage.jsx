072ad5e3270971dde276f55759eed16f69cc3731
fayssal dd <fayssaldd982@gmail.com>
2024-09-23 15:57:37 +0100

    ajouter les sevices

--- src/pages/CardMassage.jsx
+++ src/pages/CardMassage.jsx
@@ -7,7 +7,8 @@ import {
     CardHeader,
     CardTitle,
   } from "@/components/ui/card"
-export default function CardMassage({type,description,image,benefits}) {
+import { MessageCircleCode, Phone } from 'lucide-react'
+export default function CardMassage({type,description,image,price}) {
   return (
     <>
         <Card dir='rtl'  className="shadow dark:text-white text-black " >
@@ -18,10 +19,18 @@ export default function CardMassage({type,description,image,benefits}) {
             <CardContent className="flex aspect-square items-center justify-center ">
                 <img className='w-full' src={`${image}`} alt="" />
             </CardContent>
-            <CardFooter>
-                <div>
-                  <p className='font-bold underline'>فوائد</p>
-                  <p> {benefits} </p>
+            <CardFooter className={'flex justify-between items-center p-4 border-t-2'}>
+                <div className=''>
+                  <p className='text-slate-300 font-bold  line-through'>{price + 100}</p>
+                  <p className='text-red-500 font-bold font-mono'> {price} ر.س</p>
+                </div>
+                <div className='flex'>
+                  <a href='https://wa.me/12345954' >
+                    <MessageCircleCode className='bg-blue-500 text-white w-9 h-9 p-1 hover:bg-blue-300 hover:scale-125 rounded-sm mx-4'/>
+                  </a>
+                  <a href='tel:342424453'>
+                    <Phone className='bg-blue-500 w-9 h-9 p-1  text-white hover:bg-blue-300 hover:scale-125 rounded-sm '/>
+                  </a>
                 </div>
             </CardFooter>
         </Card>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import massage4 from '../assets/images/massage4.jpg'
 import Massages from './Massages'
+import { MessageCircleCode, Phone } from 'lucide-react'
 export default function Home() {
 
 
@@ -9,34 +10,43 @@ export default function Home() {
      
       <div id='raisia' 
       style={{ backgroundImage: `url(${massage4})` }} 
-      className={`grid lg:grid-cols-4 md:grid-cols-2 items-center  bg-cover lg:gap-8 gap-4 h-screen max-sm:h-[85vh] p-3`}>
+      className={`flex w-full lg:grid-cols-4 md:grid-cols-2  items-center  bg-cover lg:gap-8 gap-4 h-screen max-sm:h-[85vh] p-3`}>
 
-        <div className="max-md:order-1 max-sm:mt-9 items-center flex flex-col justify-center lg:col-span-2 md:h-screen w-full md:rounded-br-xl lg:p-12">
+        <div className="backdrop-brightness-50 backdrop-blur-sm p-7 w-full max-md:order-1 max-sm:mt-9 items-center flex flex-col justify-center lg:col-span-2  md:rounded-br-xl lg:p-12">
           <div dir='rtl' className="text-6xl pb-8 font-semibold inline-flex italic items-center text-black dark:text-white ">
-            <span className=" border-b-4 border-[#10c3da] font-mono italic ">
+            <span className=" border-b-4 border-black [text-shadow:_0_4px_8px_#00BCD4] text-[white] font-mono italic ">
             مساج 
             </span>{" "}
-            <span className="[text-shadow:_0_4px_8px_#00BCD4] text-[#00BCD4] ms-2 font-mono italic border-b-4 border-black dark:border-white">
+            <span className="[text-shadow:_0_4px_8px_#00BCD4] text-[white] ms-2 font-mono italic border-b-4 border-black dark:border-white">
               {" "}
               منزلي
             </span>
            
           </div>
-          <div dir='rtl' className='font-semibold text-lg text-white italic'>
+          <div dir='rtl' className='text-white mb-4'>
+          <b className='[text-shadow:_0_4px_8px_#00BCD4] text-xl text-[#10c3da] border-b-2 border-black text-shadow'> تجربة فاخرة لجسمك وراحتك</b> <br /> دلل نفسك بتجربة مساج مريحة وأنت في منزلك. فريقنا من الأخصائيين يوفر جلسات مساج تعالج التوتر وتمنحك راحة فورية. اختر نوع المساج المناسب لك واستمتع بالتجربة التي تستحقها.
+          </div>
+          <div className='flex'>
+            <a href='https://wa.me/12345954' >
+              <MessageCircleCode className='bg-white w-9 h-9 p-1 hover:bg-slate-300 hover:scale-125 rounded-sm mr-4'/>
+            </a>
+            <a href='tel:342424453'>
+              <Phone className='bg-white w-9 h-9 p-1 hover:bg-slate-300 hover:scale-125 rounded-sm'/>
+            </a>
+          </div>
+          {/* <div dir='rtl' className='font-semibold text-lg text-white italic'>
                 <b className='[text-shadow:_0_4px_8px_#00BCD4] text-xl text-[#10c3da] border-b-2 border-black text-sha text-blue-500'>"مساج منزلي – تجربة فاخرة لجسمك وراحتك"</b> دلل نفسك بتجربة مساج مريحة وأنت في منزلك. فريقنا من الأخصائيين يوفر جلسات مساج تعالج التوتر وتمنحك راحة فورية. اختر نوع المساج المناسب لك واستمتع بالتجربة التي تستحقها.
                 <br /><br />
                 <b className='[text-shadow:_0_4px_8px_#00BCD4] border-b-2 text-[#10c3da]  border-black text-xl '>"استمتع بالاسترخاء في منزلك مع خدمات المساج المنزلي الفاخرة"</b> تبحث عن الاسترخاء والتخلص من التوتر؟ نقدم لك تجربة مساج احترافية في راحة منزلك. احجز جلستك الآن ودعنا نعتني بجسمك وعقلك من خلال تقنيات تدليك مميزة تساعد على تنشيط الدورة الدموية وتخفيف التوتر.  
-          </div>
+          </div> */}
         </div>
-        <div className="max-md:order-1 items-center flex flex-col justify-center lg:col-span-2 md:h-screen w-full md:rounded-br-xl lg:p-12 p-8">
+        {/* <div className="max-md:order-1 items-center flex flex-col justify-center lg:col-span-2 md:h-screen w-full md:rounded-br-xl lg:p-12 p-8">
           <img
             src={massage4}
             className="lg:w-[100%] rounded-3xl object-contain block mx-auto"
           />
-          <p className="text-white font-serif">
-         
-          </p>
-        </div>
+
+        </div> */}
       </div>
     </>
   )
