0aba7025a9dcf247e4fc8bb2d154a7cd552e9e06
fayssal dd <fayssaldd982@gmail.com>
2024-09-24 17:59:54 +0100

    change titre de site

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -3,9 +3,6 @@ import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/logoa.png'
 export default function NavBar() {
   const [isShow,setIsShow] = useState(false);
-  // const location = useLocation()
-  // console.log(location.pathname);
-  
   return (
     <div>
         <nav className="backdrop-blur-sm border-b bg-white  fixed w-full shadow dark:border-b z-40">
@@ -27,24 +24,11 @@ export default function NavBar() {
             <div className="flex w-full items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex  jusfit flex-shrink-0 items-center">
                 <img className="h-8 w-auto bg-transparent mr-2" src={logo} alt="Your Company"/>
-                <p className='italic text-primary'>SPA & MASSAGE</p>
+                <p className='italic text-primary'>SPA-KANZA</p>
 
               </div>
               <div className="hidden w-4/5 sm:ml-6 sm:block">
                 <div className="flex justify-end items-center space-x-4">
-                  {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                  {/* <ModeToggle/> */}
-                  {/* <Link to={DASHBOARD_ROUTE} className={`${DASHBOARD_ROUTE == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`}>Dashboard</Link>
-                  <Link to={SALLES_ROUTE} className={`${SALLES_ROUTE == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`}>Salles</Link>
-                  <Link to={MODULES_ROUTE} className={`${MODULES_ROUTE == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`}>Modules</Link>
-                  <Link to={CLASSE_ROUTE} className={`${CLASSE_ROUTE == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`} >Classes</Link>
-                  <Link to={PROFS_ROUTE} className={`${PROFS_ROUTE == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`}>Profs</Link>
-                  <Link to={SEANCES_ROUTE} className={`${SEANCES_ROUTE == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`}>Seances</Link> */}
-                  {/* <Link to={'/#khadamtona'}className={`${'/' == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`} >Home</Link> */}
-                  {/* <Link to={'/'}className={`${'/' == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`} >Home</Link>
-                  <Link to={'/'}className={`${'/' == location.pathname && 'bg-gray-700 text-white'} rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white`} >Home</Link> */}
-                  {/* {user.name} */}
-                  {/* <DropDownMenu/> */}
                   <a className='rounded-md px-3 py-2 text-sm font-medium text-primary hover:bg-hover hover:text-white' href="#khadamtona">خدماتنا</a>
                   <a className='rounded-md px-3 py-2 text-sm font-medium text-primary hover:bg-hover hover:text-white' href="#raisia">الرئيسية</a>
                   <a className='rounded-md px-3 py-2 text-sm font-medium text-primary hover:bg-hover hover:text-white' href="#contact">
@@ -65,13 +49,6 @@ export default function NavBar() {
             <a className=' block rounded-md px-3 py-2 text-base font-medium text-primary hover:bg-hover hover:text-white' href="#khadamtona">خدماتنا</a>
             <a className=' block rounded-md px-3 py-2 text-base font-medium text-primary hover:bg-hover hover:text-white' href="#contact">تواصل
             معنا</a>
-
-            {/* <Link to={DASHBOARD_ROUTE} className={`${DASHBOARD_ROUTE == location.pathname && 'bg-gray-700 text-white'} block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white`}>Dashboard</Link>
-            <Link to={SALLES_ROUTE} className={`${SALLES_ROUTE == location.pathname && 'bg-gray-700 text-white'} block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white`}>Salles</Link>
-            <Link to={MODULES_ROUTE} className={`${MODULES_ROUTE == location.pathname && 'bg-gray-700 text-white'} block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white`}>Modules</Link>
-            <Link to={CLASSE_ROUTE} className={`${CLASSE_ROUTE == location.pathname && 'bg-gray-700 text-white'} block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white`}>Classes</Link>
-            <Link to={PROFS_ROUTE} className={`${PROFS_ROUTE == location.pathname && 'bg-gray-700 text-white'} block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white`}>Profs</Link>
-            <Link to={SEANCES_ROUTE} className={`${SEANCES_ROUTE == location.pathname && 'bg-gray-700 text-white'} block rounded-md px-3 py-2 text-base font-medium text-black hover:bg-gray-700 hover:text-white`}>Seances</Link> */}
           </div> 
           
         </div>
